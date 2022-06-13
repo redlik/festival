6cb9735deb60d7ece5ce76ccfe2ee412a7ca2003
redlik <ralphr@outlook.com>
2022-06-13 19:41:27 +0100

    Registration panel fixed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,11 +26,7 @@
         <div class="flex flex-col justify-between min-h-screen bg-gray-100">
             <div>
                 @include('layouts.header')
-                <header class="bg-white shadow" id="white-header">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+
                 <div class="font-sans text-gray-900 antialiased min-h-[640px]">
                     {{ $slot }}
                 </div>
