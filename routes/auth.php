c26279235a41f9eef805e02a7b0106bcf6cafe10
redlik <ralphr@outlook.com>
2022-06-13 19:57:00 +0100

    Registration disabled

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,9 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('register', function () {
+        return abort(403);
+    })
                 ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
