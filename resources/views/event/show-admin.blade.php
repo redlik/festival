c769bb7213ca572874db3e9738c31361142351f7
redlik <ralphr@outlook.com>
2022-06-14 05:58:37 +0100

    Admin show json

--- resources/views/event/show-admin.blade.php
+++ resources/views/event/show-admin.blade.php
@@ -60,7 +60,7 @@
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Target</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                @foreach($event->target as $target_item)
+                                @foreach(json_decode($event->target) as $target_item)
                                     <div class="gray-pillow mr-1">
                                         {{ Str::title(str_replace('-', ' ', $target_item)) }}
                                     </div>
