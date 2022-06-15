9abe8fc4a95dc18b7dfac7b2df68f05a62c07dbe
redlik <ralphr@outlook.com>
2022-06-15 20:00:07 +0100

    Homepage changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,20 +2,17 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap items-top text-gray-100">
         <div class="w-full md:w-1/3 px-4">
             <img class="block h-24 w-auto mb-2" src="{{ asset('img/logo-white.svg') }}">
-            <div class="text-gray-200 text-sm">
-                Description goes here
-            </div>
             <ul class="text-gray-200 text-sm mt-2">
                 <li class="mb-2"><span class="inline-block w-16 font-bold">Email:</span>
-                    <a href="mailto:info@kerryfest.com">info@kerryfest.com</a></li>
+                    <a href="mailto:kerrymhwfest20@gmail.com ">kerrymhwfest20@gmail.com </a></li>
             </ul>
         </div>
         <div class="w-full md:w-1/3 px-4">
             <div class="font-bold text-gray-200 mb-4 mt-8 md:mt-0">Navigation</div>
             <ul class="text-sm list-disc pl-4">
-                <li class="mb-2"><a href="">Events</a></li>
-                <li class="mb-2"><a href="">Log in</a></li>
-                <li class="mb-2"><a href="">Organiser Registration</a></li>
+                <li class="mb-2"><a href="{{ route('events') }}">Events</a></li>
+                <li class="mb-2"><a href="{{ route('login') }}">Log in</a></li>
+                <li class="mb-2"><a href="{{ route('pages.join-us') }}">Organiser Registration</a></li>
             </ul>
         </div>
         <div class="w-full md:w-1/3 px-4">
