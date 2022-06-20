ed7a468984200acd8fb96f7acbad5ea226e5d14e
redlik <ralphr@outlook.com>
2022-06-20 13:03:24 +0100

    Event details responsive

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -8,10 +8,10 @@
     <div class="py-0 md:py-12">
         <div class="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 bg-white rounded-lg shadow-lg scroll-smooth">
             <div class="grid grid-cols-5 gap-4">
-                <div class="col-span-2">
+                <div class="col-span-5 md:col-span-2">
                     <img src="{{ $event->getFirstMediaUrl('cover') }}" alt="">
                 </div>
-                <div class="col-span-3">
+                <div class="col-span-5 md:col-span-3">
                     <h4 class="font-bold text-xl">{{ $event->name }}</h4>
                     <div>
                         {{ $event->description }}
