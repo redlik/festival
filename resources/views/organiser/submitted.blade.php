129236e4ecf9d58dc23c8973cd5641d89592d012
redlik <ralphr@outlook.com>
2022-06-28 07:00:59 +0100

    Submitted page styling

--- resources/views/organiser/submitted.blade.php
+++ resources/views/organiser/submitted.blade.php
@@ -8,11 +8,11 @@
     <div class="py-0 md:py-12">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 bg-white rounded-lg shadow-lg scroll-smooth">
             <div>
-                <h2>Thank you!</h2>
-                <p>Your application has been submitted. We've sent an activation email to <strong>{{ $organiser->email }}</strong> so make sure you open it and follow the process.</p>
-                <p>In rare occassions the activation email may end up in your junk/spam folder so please check it as well.</p>
-                <p>In the meantime you can view all the events that are happening at the festival here:
-                    <a href="#" class="text-link">View all events</a></p>
+                <h2 class="mb-4">Thank you!</h2>
+                <p class="mb-2">Your application has been submitted. We've sent an activation email to <strong>{{ $organiser->email }}</strong> so make sure you open it and follow the process.</p>
+                <p class="mb-2">In rare occassions the activation email may end up in your junk/spam folder so please check it as well.</p>
+                <p class="mb-2">In the meantime you can view all the events that are happening at the festival here:
+                    <a href="/events" class="text-link">View all events</a></p>
             </div>
         </div>
     </div>
