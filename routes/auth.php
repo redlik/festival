e97eb30ec4da324a4fdc89713ee9df9f8a9db6ca
redlik <ralphr@outlook.com>
2022-07-07 07:18:27 +0100

    Registration opened

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,14 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', function () {
-        return abort(403);
-    })
-                ->name('register');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+
+//Route::middleware('guest')->group(function () {
+//    Route::get('register', function () {
+//        return abort(403);
+//    })
+//                ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
