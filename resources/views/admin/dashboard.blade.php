3c7ea67f6d1a90092f797e2fe2955bae7c9f9e36
redlik <ralphr@outlook.com>
2022-08-09 10:03:21 +0100

    Admin increased event name to 40 char

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -66,7 +66,7 @@
                             <tr>
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
                                     <a href="{{ route('admin.event.show', $event) }}" class="hover:underline">
-                                        {{ Str::of($event->name)->limit(20, ' (...)') }}
+                                        {{ Str::of($event->name)->limit(40, ' (...)') }}
                                     </a>
                                 </td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
