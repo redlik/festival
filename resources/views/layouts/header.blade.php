cb26508397efaf3ad65e9a9d0688f290c44a8bff
redlik <ralphr@outlook.com>
2022-08-09 07:45:59 +0100

    Admin organiser menu link fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -112,7 +112,7 @@
                             @endrole
                             @role('admin')
                             <a href="{{ route('admin.dashboard') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabindex="1" id="user-menu-item-1">Admin Dashboard</a>
-                            <a href="{{ route('organiser.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabindex="1" id="user-menu-item-1">Organisers</a>
+                            <a href="{{ route('admin.organisers') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabindex="1" id="user-menu-item-1">Organisers</a>
                             @endrole
                             <a href="{{ route('logout') }}"
                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem" tabindex="2"
