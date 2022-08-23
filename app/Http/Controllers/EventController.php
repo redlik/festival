39811b88a385abef2d79f3ea66c042e42e73a43c
redlik <ralphr@outlook.com>
2022-08-23 10:57:24 +0100

    Request document modal

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -256,7 +256,7 @@ class EventController extends Controller
 
     public function showAdmin($event_id)
     {
-        $event = Event::find($event_id)->with('document')->first();
+        $event = Event::has('document')->find($event_id);
 
         return view('event.show-admin', compact('event'));
     }
--- resources/views/event/show-admin.blade.php
+++ resources/views/event/show-admin.blade.php
@@ -119,8 +119,16 @@
                     </dl>
                 </div>
             </div>
-            <div class="mt-8">
-                <h6>Documents attached</h6>
+            <div class="mt-8" x-data="{ request : false }">
+                <div class="flex justify-between">
+                    <div>
+                        <h6>Documents attached</h6>
+                    </div>
+                    <div>
+                        <button @click="request = true" class="button-primary text-xs" type="button">Request documents</button>
+                    </div>
+                </div>
+
                 <div class="mt-4">
                     <table class="min-w-full border-separate" style="border-spacing: 0">
                         <thead class="bg-gray-50">
@@ -159,6 +167,58 @@
                         </tbody>
                     </table>
                 </div>
+                <!-- Modal -->
+                <div @keydown.window.escape="request = false" x-show="request" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" x-ref="dialog" aria-modal="true">
+                    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+
+                        <div x-show="request" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background overlay, show/hide based on modal state." class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="request = false" aria-hidden="true">
+                        </div>
+
+                        <!-- This element is to trick the browser into centering the modal contents. -->
+                        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">​</span>
+
+                        <div x-show="request" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-description="Modal panel, show/hide based on modal state." class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                            <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                                <button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" @click="request = false">
+                                    <span class="sr-only">Close</span>
+                                    <svg class="h-6 w-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="sm:flex sm:items-start">
+                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                        Request document(s) from the organiser
+                                    </h3>
+                                    <div class="mt-2">
+                                        <p class="text-sm text-gray-500 mb-4">
+                                            Provide a short description what documents are required.
+                                        </p>
+                                        <form action="" method="POST" class="bg-gray-100 rounded p-2" id="doc-request">
+                                            @csrf
+                                            <div class="mb-4">
+                                                <label for="message" class="text-sm text-gray-600 font-semibold mb-2">Request details:</label>
+                                                <textarea name="message" id="message" cols="30" rows="6" class="shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"></textarea>
+                                            </div>
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                <button type="submit" form="doc-request" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-olive-600 text-base font-semibold text-white hover:bg-olive-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" @click="request = false">
+                                    Send
+                                </button>
+                                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm" @click="request = false">
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
 
             </div>
         </div>
