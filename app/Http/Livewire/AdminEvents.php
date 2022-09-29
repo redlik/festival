5f115d18d1828c5c974db86c0baf75917f9c08b0
redlik <ralphr@outlook.com>
2022-09-29 15:42:36 +0100

    Pending status on admin dash events

---
+++ app/Http/Livewire/AdminEvents.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Event;
+use Livewire\Component;
+
+class AdminEvents extends Component
+{
+    public $events;
+
+    public $pending;
+
+    public function render()
+    {
+        $this->events = $events = Event::orderBy('status', 'asc')->orderBy('start_date', 'asc')
+            ->when($this->pending == 'pending', function($q) {
+                $q->where('status', 'pending');
+            })
+            ->withCount('attendee')
+            ->with('attendee', 'venue', 'user.organiser')
+            ->get();
+
+        return view('livewire.admin-events');
+    }
+}
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -24,7 +24,7 @@
                             Events added
                         </dt>
                         <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                            {{ $events->count() }}
+                            {{ $events_count }}
                         </dd>
                     </div>
 
@@ -41,112 +41,13 @@
             </div>
             <div>
                 <div class="p-6 border border-gray-200 rounded-lg mt-6 p-4 bg-white shadow-lg" id="events">
-                    <div class="flex justify-between">
+                    <div class="flex justify-between mb-4">
                         <h2 class="text-green-600">Events</h2>
                     </div>
                     @if (Session::has('deleted'))
                         <div class="bg-red-100 border border-red-700 shadow rounded p-2 my-4 text-red-600">{{ Session::get('deleted') }}</div>
                     @endif
-                    <table class="min-w-full divide-y divide-gray-300">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Name</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Date & Time</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Organiser</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Venue</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Attendees</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">Operations
-                                <span class="sr-only">Operations</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="divide-y divide-gray-200 bg-white">
-                        @forelse($events as $event)
-                            <tr>
-                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-                                    <a href="{{ route('admin.event.show', $event) }}" class="hover:underline" title="{{ $event->name }}">
-                                        {{ Str::of($event->name)->limit(40, ' (...)') }}
-                                    </a>
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    <div>{{ $event->start_date }}</div>
-                                    <div>{{ \Carbon\Carbon::parse($event->start_time)->format('H:i') }} - {{ \Carbon\Carbon::parse($event->end_time)->format('H:i') ?? "Not set" }}</div>
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    <div>
-                                        <a href="{{ route('organiser.show', $event->user->organiser->id ) }}" class="font-semibold hover:text-indigo-500 hover:underline">
-                                        {{ $event->user->organiser->name }}
-                                        </a>
-                                    </div>
-                                    <div>
-                                        {{ $event->user->organiser->org }}
-                                    </div>
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    @if($event->type != "online")
-                                        <div>{{ $event->venue->name }}</div>
-                                        <div>{{ $event->venue->town }}</div>
-                                    @else
-                                        <div>Online Event</div>
-                                    @endif
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    @if($event->is_private)
-                                        <span class="text-olive-400 bg-gray-100 rounded px-3 py-1 font-semibold"><i class="fa-solid fa-lock mr-1"></i> Private event</span>
-                                    @else
-                                        @if($event->limited == 1)
-                                            @if($event->attendees === $event->attendee_count)
-                                                <span class="font-semibold bg-red-100 text-red-700 rounded px-3 py-1">
-                                                <i class="fa-solid fa-user-lock mr-1 text-red-600"></i> {{ $event->attendee_count ?? '0' }} / {{ $event->attendees }}
-                                            </span>
-                                            @else
-                                                <span class="font-semibold bg-blue-100 text-indigo-700 rounded px-3 py-1">
-                                                <i class="fa-solid fa-user-lock mr-1 text-indigo-600"></i> {{ $event->attendee_count ?? '0' }} / {{ $event->attendees }}
-                                            </span>
-                                            @endif
-                                        @else
-                                            <span class="text-green-600 bg-green-100 rounded px-3 py-1 font-semibold"><i class="fa-solid fa-people-group mr-1"></i> {{ $event->attendee_count ?? '0' }}</span>
-                                        @endif
-                                    @endif
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    @if($event->status == 'draft')
-                                       <span class="text-amber-600 font-semibold">{{ ucfirst($event->status) }}</span>
-                                    @endif
-                                    @if($event->status == 'published')
-                                        <span class="text-green-600 font-semibold">{{ ucfirst($event->status) }}</span>
-                                    @endif
-                                    @if($event->status == 'cancelled')
-                                        <span class="text-purple-600 font-semibold">{{ ucfirst($event->status) }}</span>
-                                    @endif
-                                    @if($event->status == 'pending')
-                                        <span class="text-sky-500 font-semibold">{{ ucfirst($event->status) }}</span>
-                                    @endif
-                                </td>
-                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-center text-sm font-semibold sm:pr-6 lg:pr-8">
-                                    <a href="{{ route('admin.event.show', $event) }}" class="text-indigo-600 hover:text-indigo-900 mr-2" title="Event details"><i class="fa-solid fa-eye"></i></a>
-                                    <a href="{{ route('event.edit', $event) }}" class="text-green-600 hover:text-green-400 mr-2"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    @if($event->attendee_count > 0)
-                                        <div class="text-gray-500 inline-block cursor-not-allowed" title="Event has attendees, cannot be deleted"><i class="fa-solid fa-trash-can"></i></div>
-                                    @else
-                                        <form method="POST" action="{{ route('event.destroy', $event) }}" class="inline-block"
-                                              onsubmit="return confirm('Do you wish to delete the event completely?');">
-                                            @csrf
-                                            @method("DELETE")
-                                            <button type="submit" class="text-red-600 hover:text-red-900 hover:underline"><i class="fa-solid fa-trash-can" title="Delete event"></i></button>
-                                        </form>
-                                    @endif
-                                </td>
-                            </tr>
-                        @empty
-                            <tr colspan="4">
-                                <td><h4>No events created</h4></td>
-                            </tr>
-                        @endforelse
-
-                        </tbody>
-                    </table>
+                    @livewire('admin-events')
 
                 </div>
                 <div class="p-6 border border-gray-200 rounded-lg mt-6 p-4 bg-white shadow-lg" id="venues">
