9af1dbbfecf78b1f44ceeedfaabf4ede9a40bad2
redlik <ralphr@outlook.com>
2023-07-17 20:04:04 +0100

    Registration welcome message

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -9,6 +9,9 @@
         <div class="bg-gray-100">
             <div class="mx-auto max-w-7xl">
                 <div class="px-4 sm:px-6 lg:px-8">
+                    @if(\Illuminate\Support\Facades\Session::has('created'))
+                        <div class="bg-green-100 text-green-700 rounded p-2 mb-6 text-center">Your account has been created. You can now register for events.</div>
+                    @endif
                     @livewire('event-list')
                 </div>
             </div>
