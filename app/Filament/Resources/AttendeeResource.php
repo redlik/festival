a058a75b4f1d13fa29db499be1028cd4d8665e94
redlik <ralphr@outlook.com>
2023-08-07 06:25:44 +0100

    filament attendee resource

---
+++ app/Filament/Resources/AttendeeResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AttendeeResource\Pages;
+use App\Filament\Resources\AttendeeResource\RelationManagers;
+use App\Models\Attendee;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AttendeeResource extends Resource
+{
+    protected static ?string $model = Attendee::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('event.name')->sortable(),
+                Tables\Columns\TextColumn::make('created_at')->sortable()->label('Registered on'),
+            ])
+            ->filters([
+                SelectFilter::make('event')->relationship('event', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ])
+            ->defaultSort('event.name');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAttendees::route('/'),
+            'create' => Pages\CreateAttendee::route('/create'),
+            'edit' => Pages\EditAttendee::route('/{record}/edit'),
+        ];
+    }
+}
