c98ca4e5e3302cf894e3e65e8a9e0a970f8d5948
redlik <ralphr@outlook.com>
2023-08-08 16:48:43 +0100

    Event resource actions

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -4,28 +4,33 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\EventResource\Pages;
 use App\Filament\Resources\EventResource\RelationManagers;
+use App\Filament\Resources\EventresourceResource\RelationManagers\VenuesRelationManager;
 use App\Models\Event;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Filament\Tables\Columns\BadgeColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class EventResource extends Resource
 {
     protected static ?string $model = Event::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-pencil';
     protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name'),
+                Forms\Components\DatePicker::make('start_date')->minDate('2023-10-07')->maxDate('2023-10-14')
+                    ->disabled(! auth()->user()->hasRole('admin')),
             ]);
     }
 
@@ -34,7 +39,10 @@ class EventResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')->sortable()->searchable()->label('Event name')->limit(30),
-                Tables\Columns\TextColumn::make('start_date')->sortable()->label('Event date')->date('d M y'),
+                Tables\Columns\TextColumn::make('start_date')->sortable()->label('Event Date & Time')
+                    ->getStateUsing(function (Event $record): string {
+                        return $record->start_date .' - '. $record->start_time;
+                    }),
                 BadgeColumn::make('status')
                     ->sortable()
                     ->colors([
@@ -46,7 +54,11 @@ class EventResource extends Resource
                     ]),
             ])
             ->filters([
-                //
+                Tables\Filters\SelectFilter::make('status')->options([
+                    'draft' => 'Draft',
+                    'pending' => "Pending",
+                    'activated' => "Activated",
+                ]),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -60,7 +72,7 @@ class EventResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            VenuesRelationManager::class
         ];
     }
 
