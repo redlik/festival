19d8953ef077ecb042b5e984d1da3162af405965
redlik <ralphr@outlook.com>
2023-08-08 16:49:13 +0100

    Venue resource

---
+++ app/Filament/Resources/VenueResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VenueResource\Pages;
+use App\Filament\Resources\VenueResource\RelationManagers;
+use App\Models\Venue;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VenueResource extends Resource
+{
+    protected static ?string $model = Venue::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?int $navigationSort = 4;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('address1')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('street')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('town')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('county')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('eircode')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('website')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('address1'),
+                Tables\Columns\TextColumn::make('street'),
+                Tables\Columns\TextColumn::make('town'),
+                Tables\Columns\TextColumn::make('county'),
+                Tables\Columns\TextColumn::make('eircode')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVenues::route('/'),
+            'create' => Pages\CreateVenue::route('/create'),
+            'view' => Pages\ViewVenue::route('/{record}'),
+            'edit' => Pages\EditVenue::route('/{record}/edit'),
+        ];
+    }
+}
