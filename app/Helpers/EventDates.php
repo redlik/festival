b08c4c96eb82e0e4eb11d28b5728eb468fa13f7a
redlik <ralphr@outlook.com>
2025-07-04 14:51:18 +0100

    Event date helper

---
+++ app/Helpers/EventDates.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Event;
+use Illuminate\Support\Facades\Cache;
+
+class EventDates
+{
+  public function getEventsDates() : array
+  {
+    if (!Cache::has('events_dates')) {
+      $events_dates = Event::selectRaw('YEAR(start_date) as year')
+        ->distinct()
+        ->orderBy('year')
+        ->pluck('year')
+        ->toArray();
+      Cache::put('events_dates', $events_dates, now()->addDays(30));
+    } else {
+      $events_dates = Cache::get('events_dates');
+    }
+
+    return $events_dates;
+  }
+
+  /**
+   * Get all distinct event dates
+   *
+   * @return array
+   */
+  public function getEventsFullDates() : array
+  {
+    return $events_dates = Event::select('start_date')
+      ->distinct()
+      ->get()
+      ->toArray();
+  }
+}
