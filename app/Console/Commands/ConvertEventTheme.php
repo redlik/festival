68683fddbc242dd972fada51f85c034e3ceb6770
redlik <ralphr@outlook.com>
2025-07-08 13:25:12 +0100

    Convert event theme column

---
+++ app/Console/Commands/ConvertEventTheme.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+
+class ConvertEventTheme extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:convert-event-theme';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Convert event theme column to array';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::all();
+        foreach ($events as $event) {
+          if (!is_array(json_decode($event->theme))) {
+            $event->theme = explode(',', $event->theme);
+            $event->update();
+            ray($event->theme);
+            $this->info('Event ' . $event->id . ' updated');
+          }
+        }
+    }
+}
